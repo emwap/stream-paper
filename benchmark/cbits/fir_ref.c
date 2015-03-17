73ef32dfd016f4e982080368f22305f0e2ebc3ca
Anders Persson <anders.cj.persson@gmail.com>
2015-03-17 16:23:37 +0100

    Vary filter order instead of signal length

--- benchmark/cbits/fir_ref.c
+++ benchmark/cbits/fir_ref.c
@@ -5,24 +5,20 @@
 // Parameters
 ////////////////////////////////////////////////////////////
 
-#define NUMTAPS 4
-
-
-
-void fir_ref(uint32_t N, struct array * Signal, struct array ** FilteredSignal)
+void fir_ref(struct array * FirCoeff, struct array * Signal, struct array ** FilteredSignal)
 {
-    double const FirCoeff[NUMTAPS] = { 1.0, 0.5, 0.25, 0.125 };
-
-    double Reg[NUMTAPS];
+    int N = getLength(FirCoeff);
+    double Reg[N];
 
     // This loop body is the inlined FIR filter function from:
     // http://www.iowahills.com/A7ExampleCodePage.html
     int j, k, n, Top = 0;
     double y;
+    int len = getLength(Signal);
 
-    for(j=0; j<NUMTAPS; j++) Reg[j] = 0.0;
+    for(j=0; j<N; j++) Reg[j] = 0.0;
 
-    for(j=0; j<N; j++)
+    for(j=0; j<len; j++)
     {
       Reg[Top] = at(double, Signal, j);
       y = 0.0;
@@ -31,15 +27,15 @@ void fir_ref(uint32_t N, struct array * Signal, struct array ** FilteredSignal)
       // The FirCoeff index increases while the Reg index decreases.
       for(k=Top; k>=0; k--)
       {
-        y += FirCoeff[n++] * Reg[k];
+        y += at(double,FirCoeff,n++) * Reg[k];
       }
-      for(k=NUMTAPS-1; k>Top; k--)
+      for(k=N-1; k>Top; k--)
       {
-        y += FirCoeff[n++] * Reg[k];
+        y += at(double,FirCoeff,n++) * Reg[k];
       }
       at(double,*FilteredSignal,j) = y;
 
       Top++;
-      if(Top >= NUMTAPS)Top = 0;
+      if(Top >= N)Top = 0;
     }
 }
